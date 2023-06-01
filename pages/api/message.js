bda49564b2b614b99c5b0af2f68442ffacc059ed
Jf.Raisa <pi314raisa@gmail.com>
2023-06-01 17:49:32 +0600

    passing image in whatsapp

--- pages/api/message.js
+++ pages/api/message.js
@@ -6,38 +6,49 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openAI = new OpenAIApi(configuration);
-export default async function handler(req, res) {
-    const MessagingResponse = require('twilio').twiml.MessagingResponse;
-    var messageResponse = new MessagingResponse();
 
-    const sentMessage = req.body.Body || '';
-    let replyToBeSent = "";
-    if (sentMessage.trim().length === 0) {
-        replyToBeSent = "We could not get your message. Please try again";
+let savedImage = null; // Variable to store the image
+
+export default async function handler(req, res) {
+    const MessagingResponse = require("twilio").twiml.MessagingResponse;
+    const messageResponse = new MessagingResponse();
+  
+    const sentMessage = req.body.Body || "";
+  
+    // Check if the received message contains an image
+    if (req.body.MediaUrl0) {
+      // Save the image URL in memory
+      savedImage = req.body.MediaUrl0;
+      console.log(savedImage)
+      messageResponse.message("Image received and saved successfully.");
     } else {
+      let replyToBeSent = "";
+  
+      if (sentMessage.trim().length === 0) {
+        replyToBeSent = "We could not get your message. Please try again";
+      } else {
         try {
-            const completion = await openAI.createCompletion({
-                model: "text-davinci-003", // required
-                prompt: req.body.Body, // completion based on this
-                temperature: 0.6, //
-                n: 1,
-                max_tokens: 500,
-                // stop: "."
-            });
-            replyToBeSent = completion.data.choices[0].text 
+          const completion = await openAI.createCompletion({
+            model: "text-davinci-003",
+            prompt: sentMessage,
+            temperature: 0.6,
+            n: 1,
+            max_tokens: 500,
+          });
+  
+          replyToBeSent = completion.data.choices[0].text;
         } catch (error) {
-            if (error.response) {
-                console.log(error.response)
-                replyToBeSent = "There was an issue with the server"
-            } else { // error getting response
-                replyToBeSent = "An error occurred during your request.";
-            }
+          if (error.response) {
+            console.log(error.response);
+            replyToBeSent = "There was an issue with the server";
+          } else {
+            replyToBeSent = "An error occurred during your request.";
+          }
         }
+      }
+      console.log(replyToBeSent);
+      messageResponse.message(replyToBeSent);
     }
-    messageResponse.message(replyToBeSent);
-    // send response
-    res.writeHead(200, {
-        'Content-Type': 'text/xml'
-    });
+    res.writeHead(200, { "Content-Type": "text/xml" });
     res.end(messageResponse.toString());
-}
+  }
